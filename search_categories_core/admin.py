8ec1743a2eafe267d2061bf7281254db6d626b09
ed <edchapman88@gmail.com>
2021-05-24 18:52:53 +0100

    Add admin initial pages.

---
+++ search_categories_core/admin.py
@@ -0,0 +1,71 @@
+from django.contrib import admin
+
+
+class _BaseAdmin(admin.ModelAdmin):
+    """
+    Sections that are common to admin pages on both WS and the apps.
+    """
+    fields = [
+        "name",
+        "code",
+        "hierarchy",
+        "enabled",
+        "background_image",
+        "hd_synchronised",
+        "pro_synchronised",
+        "sub_category"
+    ]
+    readonly_fields = [
+        "code",
+        "synchronised"
+    ]
+    list_display = [
+        "hierarchy",
+        "name",
+        "code",
+        "enabled",
+        "synchronised"
+    ]
+    search_fields = [
+        "name",
+        "code"
+    ]
+    list_filter = [
+        "enabled",
+        "synchronised"
+    ]
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_view_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+        return super().has_view_permission(request)
+
+
+class WsSearchCategoryAdmin(_BaseAdmin):
+    """
+    WS SearchCategory Model admin page.
+    """
+    fields = _BaseAdmin.fields + ["product_bases"]
+    list_editable = [
+        "enabled"
+    ]
+    filter_horizontal = [
+        "product_bases"
+    ]
+
+
+class AppSearchCategoryAdmin(_BaseAdmin):
+    """
+    App SearchCategory Model admin page.
+    """
+    fields = _BaseAdmin.fields + ["products"]
+    readonly_fields = fields
