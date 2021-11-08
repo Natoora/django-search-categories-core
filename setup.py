3314b2a9b0fb021107f3888c1648fb6e082ae38a
jamesryan <j.n.ryan@hotmail.co.uk>
2021-11-08 17:34:19 +0000

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="search_categories_core",
-    version="1.0.0",
+    version="1.1.0",
     author="Ed Chapman",
     author_email="ed@natoora.com",
     description="Core functionality for the mobile app product search categories.",
