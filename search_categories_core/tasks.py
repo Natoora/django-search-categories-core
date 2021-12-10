6bf810fcf3cfa48129a4e6e2981cbb5f8811d08b
jamesryan <j.n.ryan@hotmail.co.uk>
2021-12-10 14:39:47 +0000

    Updated deployment tasks

--- search_categories_core/tasks.py
+++ search_categories_core/tasks.py
@@ -1,3 +1,14 @@
+def search_categories_deployment_tasks():
+    """
+    Run these tasks BEEFORE any db syn occurs.
+    These tasks populate the new search category format with the old data,
+    also separating out any sc that are applied across both apps.
+    """
+    populate_search_category_products()
+    populate_search_category_app_type()
+    populate_search_category_hierarchy()
+
+
 def populate_search_category_products():
     """
     On deployment, sync the old product_bases data into the new products field
@@ -19,15 +30,6 @@ def populate_search_category_products():
                     sc.save()
 
 
-# def populate_search_category_hierarchy():
-#     """
-#     On deployment, update the new app specific hierarchies with the old generic one
-#     """
-#     pass
-#     # from products.models import SearchCategory
-#     # scs = SearchCategory.objects.all()
-
-
 def populate_search_category_app_type():
     """
     On deployment, update the new app_type field with the old SC's hd_app or pro_app,
@@ -42,12 +44,9 @@ def populate_search_category_app_type():
         if (sc.hd_app and sc.pro_app) or (sc.name == 'Melilot' or sc.name == 'Melilot Farm'):
             print(f'Create a new one: {sc.name}')
             sc.app_type = sc.HD
-            # sc.hierarchy_hd = sc.hierarchy
             sc.save()
             new_sc = SearchCategory.objects.create()
             new_sc.name = sc.name
-            new_sc.hierarchy = sc.hierarchy
-            # new_sc.hierarchy_pro = sc.hierarchy
             new_sc.background_image = sc.background_image
             new_sc.tile_dimensions = sc.tile_dimensions
             new_sc.enabled = sc.enabled
@@ -64,22 +63,33 @@ def populate_search_category_app_type():
         elif sc.hd_app and not sc.pro_app:
             print(f'HD app {sc.name}')
             sc.app_type = sc.HD
-            # sc.hierarchy_hd = sc.hierarchy
             sc.save()
             continue
         elif sc.pro_app and not sc.hd_app:
             print(f'PRO app {sc.name}')
             sc.app_type = sc.PRO
-            # sc.hierarchy_pro = sc.hierarchy
             sc.save()
             continue
 
         print(f'Neither selected {sc.name}')
         sc.app_type = sc.HD
-        # sc.hierarchy_hd = sc.hierarchy
         sc.save()
 
 
+def populate_search_category_hierarchy():
+    """
+    On deployment, reset hierarchies for SC
+    """
+    from products.models import SearchCategory
+    types = ['HD', 'PRO']
 
+    for t in types:
+        scs = SearchCategory.objects.filter(app_type=t)
+        counter = 0
 
+        for index, sc in enumerate(scs):
+            print(sc, index)
+            counter += 1
+            sc.hierarchy = counter
 
+        SearchCategory.objects.bulk_update(scs, ['hierarchy'])
